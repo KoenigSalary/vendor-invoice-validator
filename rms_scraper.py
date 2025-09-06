e1b42b9fca45b071711906e1cc42a0ea6e03f006
KoenigSalary <praveen.chaudhary@koenig-solutions.com>
2025-09-06 16:28:42 +0530

    Update rms_scraper.py

--- rms_scraper.py
+++ rms_scraper.py
@@ -19,6 +19,25 @@ from selenium.common.exceptions import (
     ElementNotInteractableException
 )
 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+def setup_chrome_driver():
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--window-size=1920,1080')
+    
+    # Use webdriver-manager to automatically handle ChromeDriver
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    
+    return driver
+
 load_dotenv()
 
 RMS_URL = os.getenv('RMS_URL')
