c5701f188dafb7ac42ab9739e4a0762a03486c0d
KoenigSalary <praveen.chaudhary@koenig-solutions.com>
2025-10-22 15:34:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='vendor_invoice_validator',
-    version='0.1',
+    name="your-package-name",
+    version="0.1",
     packages=find_packages(),
     install_requires=[
-        # Add your dependencies here
-        'pandas>=2.3.0',
-        'numpy>=1.26.0',
-        'openpyxl>=3.1.1',
-        'selenium>=4.21.0',
-        # Add more dependencies as needed
+        # your dependencies
     ],
 )
+
