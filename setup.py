9b034c7c59f86a246adb1b3b00e6935dc558245e
KoenigSalary <praveen.chaudhary@koenig-solutions.com>
2025-10-22 15:22:49 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+  from setuptools import setup
+
+  setup(
+      name='vendor_invoice_validator',
+      version='0.1',
+      packages=['vendor_invoice_validator'],
+      install_requires=[
+          # List your dependencies here
+      ],
+  )
